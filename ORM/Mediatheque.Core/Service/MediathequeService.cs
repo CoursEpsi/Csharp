2cc97704218e2d543639287b2936a7627b57b025
William Dourlens <william.dourlens@ecoles-epsi.net>
2024-04-16 15:16:51 +0200

    Ajout de fonctions de CRUD

--- ORM/Mediatheque.Core/Service/MediathequeService.cs
+++ ORM/Mediatheque.Core/Service/MediathequeService.cs
@@ -1,4 +1,5 @@
-﻿using Mediatheque.Core.Model;
+﻿using Mediatheque.Core.DAL;
+using Mediatheque.Core.Model;
 
 namespace Mediatheque.Core.Service
 {
@@ -6,10 +7,16 @@ namespace Mediatheque.Core.Service
     {
         private List<ObjetDePret> _fondDeCommerce = new List<ObjetDePret>();
         private INotationService _notationService;
+        private ApplicationDbContext _db;
 
         public MediathequeService(INotationService notationService)
         {
             _notationService = notationService;
+        }
+        public MediathequeService(INotationService notationService, ApplicationDbContext applicationDbContext)
+        {
+            _notationService = notationService;
+            _db = applicationDbContext;
         }
 
         public void AddObjet(ObjetDePret objet)
@@ -39,6 +46,42 @@ namespace Mediatheque.Core.Service
             }
 
             return listeCD;
+        }
+
+        public List<CD> GetCDs()
+        {
+            return this._db.CDs.ToList();
+        }
+
+        public CD AddCd(CD cd)
+        {
+            this._db.CDs.Add(cd);
+            this._db.SaveChanges();
+            return cd;
+        }
+
+        public CD GetCdById(int cdId)
+        {
+            return this._db.CDs.Find(cdId);
+        }
+
+        public List<CD> GetCdsByGroupe(string groupe)
+        {
+            return this._db.CDs.Where(cd => cd.Groupe == groupe).ToList();
+        }
+
+        public void DeleteCd(int cdId)
+        {
+            var cd = this._db.CDs.Find(cdId);
+            this._db.CDs.Remove(cd);
+            this._db.SaveChanges();
+        }
+
+        public void EditCd(int cdId, string albumModified)
+        {
+            var cd = this._db.CDs.Find(cdId);
+            cd.TitreDeLObjet = albumModified;
+            this._db.SaveChanges();
         }
     }
 }
